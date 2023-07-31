5e4d082af35171bf3324fe8c5c2cbb49939c0445
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-31 22:11:09 +0200

    Signing Cookies

--- Section_47_Express_Router_&_Cookies/Router_and_Cookies/CookiesDemo/index.js
+++ Section_47_Express_Router_&_Cookies/Router_and_Cookies/CookiesDemo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 
-app.use(cookieParser())
+app.use(cookieParser("thisismysecret"))
 
 app.get('/greet', (req, res) => {
     const { name = "Dari" } = req.cookies;
@@ -14,6 +14,15 @@ app.get('/setname', (req, res) => {
     res.cookie('name', 'Obi Wan')
     res.cookie('animal', 'SPINE')
     res.send('HERE IS YOUR COOKIE!')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true })
+    res.send('OK SIGNED UR COOKIES')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, () => {
