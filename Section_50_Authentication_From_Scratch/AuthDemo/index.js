870fcff1a46a9eea2278c4f2ba43630dbb8f43d5
Hadjati Darius <hadjati.darius90@gmail.com>
2023-08-03 14:30:05 +0200

    Auth Demo: Registering

--- Section_50_Authentication_From_Scratch/AuthDemo/index.js
+++ Section_50_Authentication_From_Scratch/AuthDemo/index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/authDemo')
+    .then(() => {
+        console.log('MONGO CONNECTION OPEN!!!');
+    })
+    .catch((err) => {
+        console.log('OH NO MONGO CONNECTION ERROR!!!!');
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOMEPAGE');
+})
 app.get('/register', (req, res) => {
     res.render('register');
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
