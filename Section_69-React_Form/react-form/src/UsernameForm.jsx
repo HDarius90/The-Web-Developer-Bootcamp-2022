db01233877cef8d30ed08fe5dfd289e7e430f6f6
Darius Hadjati <darius.hadjati@gmail.com>
2023-08-22 10:14:30 +0200

    Controlled Components

---
+++ Section_69-React_Form/react-form/src/UsernameForm.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+export default function UsernameForm() {
+    const [username, setUsername] = useState('timmy');
+    const updateUsername = (evt) => {
+        setUsername(evt.target.value)
+    }
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={updateUsername} />
+            <button>Submit</button>
+        </div>
+    )
+}
