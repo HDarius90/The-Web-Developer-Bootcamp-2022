a708621e5c75c0bf12189f053db4ed9307f48583
Darius Hadjati <darius.hadjati@gmail.com>
2023-08-23 15:00:58 +0200

    Adding a Loader

--- Section_70-Effects/effects/src/App.jsx
+++ Section_70-Effects/effects/src/App.jsx
@@ -1,15 +1,22 @@
-import './App.css'
-import Counter from './Counter'
-import QuoteFetcher from './QuoteFetcher'
+import "./App.css";
+import QuoteFetcher from "./QuoteFetcher";
+import Counter from "./counter";
+import QuoteFetcherLoader from "./QuoteFetcherLoader";
+
 
 function App() {
-
-
   return (
     <>
+      <h1>Counter</h1>
+      <Counter />
+      <hr />
       <QuoteFetcher />
+      <hr />
+      <QuoteFetcherLoader />
+      <hr />
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
