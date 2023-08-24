e6c9c1c9b4d458a5b86c64e192bb235e25b0773f
Darius Hadjati <darius.hadjati@gmail.com>
2023-08-24 17:54:21 +0200

    introducing material ui

---
+++ Section_71-Material_UI/material-ui-demo/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Stack spacing={2} direction="row">
+          <Button color="error" variant="text">Text</Button>
+          <Button color="success" variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
+      </div>
+    </>
+  )
+}
+
+export default App
